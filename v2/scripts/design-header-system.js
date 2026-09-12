009ebd535eb40cdc34035fdca7de39670a75c81b
kasparito67 <simoncaspar5@gmail.com>
2026-09-12 16:01:40 -0400

    Allow shared desktop header in forced preview mode

--- v2/scripts/design-header-system.js
+++ v2/scripts/design-header-system.js
@@ -1,6 +1,6 @@
 (()=>{
-  const desktop=matchMedia('(min-width:701px) and (hover:hover) and (pointer:fine)');
-  if(!desktop.matches)return;
+  const desktop=window.__ASWA40_FORCE_DESKTOP__===true||matchMedia('(min-width:701px) and (hover:hover) and (pointer:fine)').matches;
+  if(!desktop)return;
 
   const esc=s=>String(s??'').replace(/[&<>'\"]/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;',"'":'&#39;','\"':'&quot;'}[c]));
   const mediaConfig=window.ASWA40_HEADER_MEDIA||{};
